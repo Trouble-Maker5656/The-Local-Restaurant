df6b522084213bc467d96399f0fdeb4ec6036d99
Trouble-Maker5656 <nerojr168@gmail.com>
2026-09-22 18:13:45 +0700

    create feedback and do responsive

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ menuOpenButton.addEventListener("click", () => {
 });
 // ====close menu====
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// ====swiper====
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  sapceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+
+});
